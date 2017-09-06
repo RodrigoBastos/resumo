4800b0c9c324c3b55d6642cfde6bee2e6c9904a7
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2017-09-06 01:23:41 -0300

    Verifica matche e executa page action

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,45 @@
 'use strict';
 
-class ExtensionBackground {
-  constructor() { this.events(); }
+// class ExtensionBackground {
+//   constructor() { this.events(); }
+//
+//   events() {
+//     chrome.browserAction.onClicked.addListener((tab) => {
+// 			chrome.extension.getBackgroundPage().console.log('Execute Background');
+// 			chrome.tabs.executeScript({
+// 				code: 'document.body'
+// 			}, (result) => {
+// 				chrome.extension.getBackgroundPage().console.log('result', result);
+//       });
+//     });
+//   }
+// }
+//
+// new ExtensionBackground();
 
-  events() {
-    chrome.browserAction.onClicked.addListener((tab) => {
-      alert('Resumo de links!');
-    });
-  }
+class ExtensionBackground {
+	constructor() {
+		// Quando estiver instalando/atualizando:
+		chrome.runtime.onInstalled.addListener(this.events);
+	}
+
+	events() {
+		// Remover as regras de mudança de página:
+		chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+			// Para adicionar a regra abaixo:
+			chrome.declarativeContent.onPageChanged.addRules([{
+				// Se:
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						// A url da página conter:
+						pageUrl: { urlContains: 'facebook.com' },
+					}) ],
+				// Nesse caso, mostramos a Page Action da extensão:
+				actions: [ new chrome.declarativeContent.ShowPageAction() ]
+			}]);
+		});
+	}
 }
 
 new ExtensionBackground();
+
